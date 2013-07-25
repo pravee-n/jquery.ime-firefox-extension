e1bbf770aee6b9d899d02ce17ea8043d0265cda8
Praveen <prageck@gmail.com>
2013-07-25 14:15:04 +0530

    removed firefox specific code from submodules

--- data/js/jquery.ime.init.js
+++ data/js/jquery.ime.init.js
@@ -48,7 +48,7 @@ $( document ).ready( function () {
 				var $ulsTrigger;
 	
 				$ulsTrigger = $( '<a>' ).text( '...' )
-					.addClass( 'ime-selector-more-languages' );
+					.addClass( 'ime-selector-more-languages selectable-row selectable-row-item' );
 	
 				$ulsTrigger.uls( {
 					onSelect: function ( language ) {
@@ -64,5 +64,41 @@ $( document ).ready( function () {
 				return $ulsTrigger;
 			}
 		} );
+
+		$input.data( 'ime' ).constructor.prototype.load = function ( inputmethodId ) {
+			var ime = this,
+				deferred = $.Deferred(),
+				dependency,
+				runtimeOrExtension;
+
+			if ( $.ime.inputmethods[inputmethodId] ) {
+				return deferred.resolve();
+			}
+
+			dependency = $.ime.sources[inputmethodId].depends;
+			if ( dependency ) {
+				return $.when( this.load( dependency ), this.load( inputmethodId ) );
+			}
+
+			// notify addon script to inject required file
+			self.port.emit( "injectScript", $.ime.sources[inputmethodId].source );
+
+			// Callback function from addon script to notify whether
+			// script injected successfully or not.
+			self.port.on( "injectSciptCallback", function ( response ) {
+				if ( response.injected ) {
+					// script injected successfully
+					eval( response.scriptToInject );
+					console.log( name + ' loaded' );
+					deferred.resolve();
+				} else {
+					// some error occured while injecting script
+					console.log( 'Error in loading inputmethod ' + name + ' Error: ' + response.errorMessage );
+				}
+				
+			} );
+
+			return deferred.promise();
+		}
 	} );
 } );
