a8e90ebd2107786119e6f646b6d54880cdef6df9
Praveen <prageck@gmail.com>
2013-07-30 23:16:42 +0530

    using extension preference rather than site specific local storage

--- data/resources/js/jquery.ime.init.js
+++ data/resources/js/jquery.ime.init.js
@@ -1,17 +1,24 @@
-// var simpleStorage = require("sdk/simple-storage");
-
 $( document ).ready( function () {
 	// Extend the ime preference system
 	$.extend( $.ime.preferences, {
 
 		save: function () {
-			// simpleStorage.storage.imepreferences = JSON.stringify( this.registry );
-			localStorage.setItem( 'imepreferences', JSON.stringify( this.registry ) );
+			self.port.emit( 'savePreferences', JSON.stringify( this.registry ) );
+			// localStorage.setItem( 'imepreferences', JSON.stringify( this.registry ) );
 		},
 
 		load: function () {
 			// this.registry = JSON.parse( simpleStorage.storage.imepreferences ) || this.registry;
-			this.registry = JSON.parse( localStorage.getItem( 'imepreferences' ) ) || this.registry;
+			var imePreferences = this;
+			self.port.emit( 'loadPreferences' );
+
+			self.port.on( 'loadPreferencesCallback', function ( response ) {
+				if ( response.imePreferences != null ) {
+					imePreferences.registry = JSON.parse( response.imePreferences ) || imePreferences.registry;
+				}
+
+				initializeIME();
+			} );
 		}
 
 	} );
@@ -44,77 +51,80 @@ $( document ).ready( function () {
 	// Load the ime preferences
 	$.ime.preferences.load();
 	
-	$( 'body' ).on( 'focus.ime', 'input:not([type]), input[type=text], input[type=search], textarea', function () {
-		var $input = $( this );
-		$input.ime( {
-			languages: quickList(),
-			languageSelector: function () {
-				var $ulsTrigger;
-	
-				$ulsTrigger = $( '<a>' ).text( '...' )
-					.addClass( 'ime-selector-more-languages selectable-row selectable-row-item' );
-	
-				$ulsTrigger.uls( {
-					onSelect: function ( language ) {
-						$input.data( 'imeselector' ).selectLanguage( language );
-						$input.focus();
-					},
-					lazyload: false,
-					languages: availableLanguages()
-					// top: $( window ).height()/2 - 214,
-					// left: $( window ).width()/2 - 358
-				} );
-	
-				return $ulsTrigger;
-			},
-			helpHandler: function ( ime ) {
-				return $( '<a>' )
-					.attr( {
-						href: 'https://www.mediawiki.org/wiki/Special:MyLanguage/Help:Extension:UniversalLanguageSelector/Input_methods/$1'.replace( '$1', ime ),
-						target: '_blank',
-						title: 'How to use'
-					} )
-					.addClass( 'ime-perime-help' )
-					.click( function ( event ) {
-						event.stopPropagation();
+	function initializeIME() {
+
+		$( 'body' ).on( 'focus.ime', 'input:not([type]), input[type=text], input[type=search], textarea', function () {
+			var $input = $( this );
+			$input.ime( {
+				languages: quickList(),
+				languageSelector: function () {
+					var $ulsTrigger;
+		
+					$ulsTrigger = $( '<a>' ).text( '...' )
+						.addClass( 'ime-selector-more-languages selectable-row selectable-row-item' );
+		
+					$ulsTrigger.uls( {
+						onSelect: function ( language ) {
+							$input.data( 'imeselector' ).selectLanguage( language );
+							$input.focus();
+						},
+						lazyload: false,
+						languages: availableLanguages()
+						// top: $( window ).height()/2 - 214,
+						// left: $( window ).width()/2 - 358
 					} );
-			}
-		} );
-
-		$input.data( 'ime' ).constructor.prototype.load = function ( inputmethodId ) {
-			var ime = this,
-				deferred = $.Deferred(),
-				dependency,
-				runtimeOrExtension;
-
-			if ( $.ime.inputmethods[inputmethodId] ) {
-				return deferred.resolve();
-			}
-
-			dependency = $.ime.sources[inputmethodId].depends;
-			if ( dependency ) {
-				return $.when( this.load( dependency ), this.load( inputmethodId ) );
-			}
-
-			// notify addon script to inject required file
-			self.port.emit( 'injectScript', $.ime.sources[inputmethodId].source );
-
-			// Callback function from addon script to notify whether
-			// script injected successfully or not.
-			self.port.on( 'injectSciptCallback', function ( response ) {
-				if ( response.injected ) {
-					// script injected successfully
-					eval( response.scriptToInject );
-					console.log( name + ' loaded' );
-					deferred.resolve();
-				} else {
-					// some error occured while injecting script
-					console.log( 'Error in loading inputmethod ' + name + ' Error: ' + response.errorMessage );
+		
+					return $ulsTrigger;
+				},
+				helpHandler: function ( ime ) {
+					return $( '<a>' )
+						.attr( {
+							href: 'https://www.mediawiki.org/wiki/Special:MyLanguage/Help:Extension:UniversalLanguageSelector/Input_methods/$1'.replace( '$1', ime ),
+							target: '_blank',
+							title: 'How to use'
+						} )
+						.addClass( 'ime-perime-help' )
+						.click( function ( event ) {
+							event.stopPropagation();
+						} );
 				}
-				
 			} );
 
-			return deferred.promise();
-		}
-	} );
+			$input.data( 'ime' ).constructor.prototype.load = function ( inputmethodId ) {
+				var ime = this,
+					deferred = $.Deferred(),
+					dependency,
+					runtimeOrExtension;
+
+				if ( $.ime.inputmethods[inputmethodId] ) {
+					return deferred.resolve();
+				}
+
+				dependency = $.ime.sources[inputmethodId].depends;
+				if ( dependency ) {
+					return $.when( this.load( dependency ), this.load( inputmethodId ) );
+				}
+
+				// notify addon script to inject required file
+				self.port.emit( 'injectScript', $.ime.sources[inputmethodId].source );
+
+				// Callback function from addon script to notify whether
+				// script injected successfully or not.
+				self.port.on( 'injectSciptCallback', function ( response ) {
+					if ( response.injected ) {
+						// script injected successfully
+						eval( response.scriptToInject );
+						console.log( name + ' loaded' );
+						deferred.resolve();
+					} else {
+						// some error occured while injecting script
+						console.log( 'Error in loading inputmethod ' + name + ' Error: ' + response.errorMessage );
+					}
+					
+				} );
+
+				return deferred.promise();
+			}
+		} );
+	}
 } );
